ffbcb1cbe0a80169036f81ea5b404a2990de27f0
Matthew Egan <matthewj.egan@hotmail.com>
2019-01-02 19:45:41 +1100

    add initial implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import mccs from './mccs.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: ''
+    }
+  }
+
+  handleInputChange = (event) => {
+    this.setState({
+      search: event.target.value
+    });
+  }
+
   render() {
+    const mccMatch = this.state.search ? mccs.filter((mccObj) => mccObj.mcc === this.state.search) : [];
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>MCC Look Up</h1>
+        <h2>Look up any <a href="https://en.wikipedia.org/wiki/Merchant_category_code">Merchant Category Code</a></h2>
+
+        <input name="Search" placeholder="Search..." type="number" value={this.state.search} onChange={this.handleInputChange}/>
+
+        { mccMatch.length !== 0 ? (
+          <p>{mccMatch[0].description}</p>
+        ) : null }
+
       </div>
     );
   }
